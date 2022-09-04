2b3acf106544c9e9d6611d54553c990b4e19c70c
Tamilo4ka <sone4ko2019@gmail.com>
2022-09-04 10:00:58 +0300

    Added the basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Weather.css";
-
 export default function Weather() {
   return (
     <div className="Weather">
@@ -11,31 +10,42 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Paris</h1>
       <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
+        <li>Wednesday 12:00</li>
+        <li> Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">22</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
+            <li>Precipitation: 25%</li>
+            <li>Humidity: 62%</li>
+            <li>Wind: 38 km/h</li>
           </ul>
         </div>
       </div>
