1b6f81324e9656111661f30312897ec2a9d330ef
Tamilo4ka <sone4ko2019@gmail.com>
2022-09-04 09:33:14 +0300

    Added the link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tamila Bichurina and is{" "}
+        <a
+          href="https://github.com/Tamilo4ka/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
